67f9ba1d7f7474eced843703c6e08fa1abb77190
ystrtnkl <freewillfooljoy@gmail.com>
2025-09-24 16:47:28 +0200

    a

--- Ejercicios3-04/main.js
+++ Ejercicios3-04/main.js
@@ -1,2 +1,16 @@
 "use strict";
-import { x } from "./ejercicio1.js";
+import { ejercicio1 } from "./bibliotecas/ejercicio1.js";
+import { ejercicio2 } from "./bibliotecas/ejercicio2.js";
+
+//Código para el ejercicio 1:
+ejercicio1();
+
+//Código para el ejercicio 2:
+ejercicio2();
+
+//Código para el ejercicio 3:
+
+
+
+
+
