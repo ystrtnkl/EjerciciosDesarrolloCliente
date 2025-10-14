f672a8ea169466e778cf60116f7cc75e80f1e05e
ystrtnkl <freewillfooljoy@gmail.com>
2025-10-14 17:40:25 +0200

    a

--- Ejercicios4-02/main02.js
+++ Ejercicios4-02/main02.js
@@ -1,8 +1,27 @@
 "use strict";
-import { x } from "./bibliotecas";
+import { esPrimo } from "./bibliotecas/ejercicio2.js";
 
 //Código para el ejercicio 2:
+//La tabla se crea desde 0 completamente con JavaScript en lugar de usar una ya existente en el HTML.
+const tabla = document.getElementById("contenedor-tabla").appendChild(document.createElement("table"));
+tabla.setAttribute("id", "tabla-principal");
+for (let i = 0; i < 10; i++) {
+    const fila = tabla.appendChild(document.createElement("tr"));
+    for (let ii = 1; ii < 11; ii++) {
+        let valor = ii + (i * 10);
+        fila.innerHTML += `<td>${valor}</td>`;
+    }
+}
 
 setTimeout(() => {
-
+    const tabla = document.getElementById("tabla-principal");
+    const filas = Array.from(tabla.children); //Convirtiendo el HTMLCollection en Array para un mejor manejo.
+    filas.map((e, i) => {
+        const celdas = Array.from(e.children);
+        celdas.map((ee, ii) => {
+            if (esPrimo(parseInt(ee.innerHTML))) {
+                ee.classList.add("celda-prima");
+            }
+        });
+    });
 }, 2000);
--- Ejercicios4-02/main04.js
+++ Ejercicios4-02/main04.js
@@ -1,7 +1,27 @@
 "use strict";
-import { x } from "./bibliotecas";
 
 //Código para el ejercicio 4:
 setTimeout(() => {
+    const imagenes = [
+        "https://i.ibb.co/prPQL0rm/imagen1.png",
+        "https://i.ibb.co/z1rQ209/imagen2.png",
+        "https://i.ibb.co/x8PQjhwD/imagen3.png",
+        "https://i.ibb.co/mF6nfmxg/imagen4.png"
+    ]; //El código está adaptado para soportar cualquier número de imágenes.
+    const imagen = document.getElementById("imagen");
+    imagen.setAttribute("src", imagenes[0]);
+    let indice = 1;
+    setInterval(() => {
+        imagen.setAttribute("src", imagenes[indice]);
+        indice++;
+        if (indice >= imagenes.length) {
+            indice = 0;
+        }
 
+        //Manera de llamar manualmente a la animación, ya que CSS no detecta los cambios de src en el <img>.
+        imagen.classList.add("animar");
+        setTimeout(() => {
+            imagen.classList.remove("animar");
+        }, 500);
+    }, 2000);
 }, 2000);
