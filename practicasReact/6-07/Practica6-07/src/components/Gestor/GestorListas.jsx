8e63b9afa5dc1c0f1b09e01930dfea449156b98a
ystrtnkl <freewillfooljoy@gmail.com>
2026-01-19 20:25:12 +0100

    a

---
+++ practicasReact/6-07/Practica6-07/src/components/Gestor/GestorListas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GestorListas() {
+
+  return (
+    <div>
+        <p>Gestor de listas, este componente solo es accesible a usuarios con la sesión iniciada</p>
+    </div>
+  );
+}
+
+export default GestorListas;
--- practicasReact/6-07/Practica6-07/src/pages/Gestor.jsx
+++ practicasReact/6-07/Practica6-07/src/pages/Gestor.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useSesion from '../hooks/useSesion.js';
+import GestorListas from '../components/Gestor/GestorListas.jsx';
 
 function Gestor() {
+
+  const navegar = useNavigate();
+  const { sesionIniciada } = useSesion();
+  useEffect(() => {
+    if (!sesionIniciada) navegar("/login");
+  }, []);
 
   return (
     <>
         <h2>Gestor de listas de la compra</h2>
+        {sesionIniciada && (<GestorListas />)}
     </>
   )
 }
